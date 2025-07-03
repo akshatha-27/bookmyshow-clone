3c43d075d8dd66b28bd387b03e4834457156cefc
akshatha-27 <nayakakshatha27@gmail.com>
2025-07-04 00:11:59 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<HomePage></HomePage>}></Route>
+      <Route path="/movie/:id" element={<MoviePage></MoviePage>}></Route>
+      <Route path="/plays" element={<Plays></Plays>}></Route> */}
+    </Routes>
+  );
+}
+
+export default App;
